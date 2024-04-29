77cff64c69a3b63f90f348d847649c76723eaf9d
suresh1610 <sureshkanthasamy39@gmail.com>
2024-04-29 16:44:40 +0530

    without signuo & login files

--- signup_and_login.py
+++ signup_and_login.py
@@ -2,35 +2,180 @@
 
 import tkinter as tk
 from tkinter import ttk
-from login import todo_login
-from signup import signup
+# from login import user_login
+# from signup import signup
+from tkinter import *
+from tkinter import messagebox
+# from signup import signup
+import mysql.connector
+import psycopg2
+import re
+from todo import task
 from ctypes import windll
 
 windll.shcore.SetProcessDpiAwareness(1)
 
 def open_login_window():
-    todo_login()
+    main_root.destroy()
+    user_login()
 
 def open_signup_window():
-    signup()
+    main_root.destroy()
+    user_signup()
 
-root = tk.Tk()
-root.title("Login / Sign Up")
-root.geometry('500x200')
-root.resizable(False, False)
+def user_login():
+    def check_user():
+        username = username_entry.get()
+        password = password_entry.get()
 
-login_label = ttk.Label(root, text="if you already have account --->")
+
+        connection = mysql.connector.connect(  # here i am connect to database
+            host="localhost",
+            user="root",
+            passwd="",
+            database="pumo_project"
+        )
+
+        pointer = connection.cursor()
+        query = 'select * from userdetails where username = %s and password = %s'
+        data = (username, password)
+        pointer.execute(query, data)
+        user_detail = pointer.fetchall()
+
+        if user_detail:
+            messagebox.showinfo('login successfully', "welcome "+ username + " !!!")
+            login_window.destroy()
+            task(username)
+         # Here i calling task function from todo.py and pass the username to that file
+        else:
+            messagebox.showinfo('Login failed', "Invalid username and password")
+            username_entry.delete(0, END)
+            password_entry.delete(0, END)
+    
+    login_window = tk.Tk()
+    login_window.geometry('500x200')
+    login_window.title('Login')
+    login_window.resizable(False, False)
+
+    username_label = ttk.Label(login_window, text="Enter your username:")
+    username_label.grid(column=4, row=2, padx=10, pady=10)
+
+    username_entry = ttk.Entry(login_window)
+    username_entry.grid(column=5, row=2, padx=10, pady=10)
+    username_entry.focus()
+
+    password_label = ttk.Label(login_window, text="Enter your password:")
+    password_label.grid(column=4, row=3, padx=10, pady=10)
+
+    password_entry = ttk.Entry(login_window, show='*')
+    password_entry.grid(column=5, row=3, padx=10, pady=10)
+
+    login_btn = ttk.Button(login_window, text='Login', command=check_user)
+    login_btn.grid(column=5, row=4, padx=10, pady=10)
+
+    register_label = ttk.Label(login_window, text="if you don't have account--->", foreground="blue")
+    register_label.grid(column=4, row=5, padx=10, pady=10)
+
+    register_btn = ttk.Button(login_window, text="register", command=user_signup)
+    register_btn.grid(column=5, row=5, padx =10, pady=10)
+
+    login_window.mainloop()
+
+def user_signup():
+    def validate_username(username):
+        return bool(re.match(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$', username)) # Username must contain at least one letter and one number
+
+    def validate_password(password):
+        # Password must contain at least one letter, one number, and one special character
+        return bool(re.match(r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]+$', password)) 
+
+    def register():
+        username = username_entry.get()
+        email = email_entry.get()
+        password = password_entry.get()
+
+        if not validate_username(username):
+            messagebox.showerror("sign up", "Username and password must contain at least one letter, one number and special character")
+            return
+        elif not validate_password(password):
+            messagebox.showerror("sign up", "Password must contain at least one letter, one number and special character")
+            return
+
+        elif validate_username(username) and validate_password(password):
+            try:
+                insert_query = 'insert into userdetails(username, email, password) values(%s, %s, %s)'
+                date = (username, email, password)
+                pointer.execute(insert_query, date)
+                connection.commit()
+                messagebox.showinfo("Sign up","your data is successfully registered")
+                signup_window.destroy()
+                task(username)  # Here i calling task function from todo.py and pass the username to that file
+                return
+            except mysql.connector.errors.IntegrityError as error:
+                messagebox.showinfo("sign up", "The username already exist, Please choose a different username")
+                signup_window.destroy()
+                # signup()
+                return
+    
+    connection = mysql.connector.connect(  # here i am connect to database
+        host="localhost",
+        user="root",
+        passwd="",
+        database="pumo_project"
+    )
+
+    pointer = connection.cursor() # creating cursor
+
+    table_creation_query = 'create table if not exists userdetails(username varchar(30) primary key, email varchar(40), password varchar(15))'
+    pointer.execute(table_creation_query)
+    connection.commit()
+
+# Create signup window
+    signup_window = tk.Tk()
+    signup_window.title("Sign Up")
+    signup_window.resizable(False, False)
+    signup_window.geometry('500x200')
+    
+    username_label = ttk.Label(signup_window, text="Username:")
+    username_label.grid(column=3, row=0, padx=10, pady=10)
+
+    username_entry = ttk.Entry(signup_window)
+    username_entry.grid(column=4, row=0, padx=10, pady=10)
+
+    email_label = ttk.Label(signup_window, text="Email:")
+    email_label.grid(column=3, row=1, padx=10, pady=10)
+
+    email_entry = ttk.Entry(signup_window)
+    email_entry.grid(column=4, row=1, padx=10, pady=10)
+
+    password_label = ttk.Label(signup_window, text="Password:")
+    password_label.grid(column=3, row=2, padx=10, pady=10)
+
+    password_entry = ttk.Entry(signup_window, show="*")
+    password_entry.grid(column=4, row=2, padx=10, pady=10)
+
+    signup_button = ttk.Button(signup_window, text="Sign Up", command=register)
+    signup_button.grid(column=4, row=3, padx=10, pady=10)
+
+    signup_window.mainloop()
+
+main_root = Tk()
+main_root.title("Login / Sign Up")
+main_root.geometry('500x200')
+main_root.resizable(False, False)
+
+login_label = ttk.Label(main_root, text="if you already have account --->")
 login_label.place(relx=0.4, rely=0.3, anchor="center")
 
-login_button = ttk.Button(root, text="Login", command=open_login_window)
+login_button = ttk.Button(main_root, text="Login", command=open_login_window)
 login_button.place(relx=0.75, rely=0.3, anchor="center")
 
-signup_label = ttk.Label(root, text="if you don't have account --->")
+signup_label = ttk.Label(main_root, text="if you don't have account --->")
 signup_label.place(relx=0.4, rely=0.5, anchor="center")
 
-signup_button = ttk.Button(root, text="Sign Up", command=open_signup_window)
+signup_button = ttk.Button(main_root, text="Sign Up", command=open_signup_window)
 signup_button.place(relx=0.75, rely=0.5, anchor="center")
 
-root.mainloop()
+main_root.mainloop()
 
 
--- todo.py
+++ todo.py
@@ -73,6 +73,11 @@ def task(username):  # main function
 
             complete_btn = ttk.Button(task_frame, text="todo completed", command=lambda task_id=task_id: update_task(task_id))
             complete_btn.pack(side="left")
+    
+    def clear_frame():
+        for frames in [today_frame, tomorrow_frame, previous_frame, complted_frame]:
+            for widget in frames.winfo_children():
+                widget.destroy()
 
     def add_task(): # insert entered data into the db (sub function)
 
@@ -85,15 +90,18 @@ def task(username):  # main function
         data = (enter_task, date, complete, u_name)
         pointer.execute(query, data)
         conn.commit()
+        clear_frame()
+        reload_task()
     
     def update_task(task_id):
 
         complete_status = True
-
         query = "update todo set completion_status=%s where task_id=%s"
         data = (complete_status,task_id)
         pointer.execute(query, data)
         conn.commit()
+        clear_frame()
+        reload_task()
         print("data is updated")
     
     def edit_task_name(task_id, new_task_name):
@@ -101,6 +109,8 @@ def task(username):  # main function
         edit_data = (new_task_name, task_id)
         pointer.execute(edit_query, edit_data)
         conn.commit()
+        clear_frame()
+        reload_task()
 
     
     def edit_task(task_id):
@@ -116,6 +126,8 @@ def task(username):  # main function
             data = (task_id,)
             pointer.execute(delete_query, data)
             conn.commit()
+            clear_frame()
+            reload_task()
             messagebox.showinfo("delete message", "Todo deleted successfully")
         else:
             messagebox.showinfo("delete message", "Todo deletion failed")
@@ -126,42 +138,57 @@ def task(username):  # main function
         # clear_frames()
         complete_status = 1
 
-        query = "select * from todo where completion_status=%s"
-        data = (complete_status,)
+        query = "select * from todo where completion_status=%s and u_name=%s"
+        data = (complete_status,username)
         pointer.execute(query, data)
         todos = pointer.fetchall()
 
         for todo_ele in todos:
             task_id, task_name, task_date, completion_status, u_name = todo_ele
 
-            completed_label = Label(complted_frame, text=task_name)
-            completed_label.pack(side="left")
+            complete_frame = Frame(complted_frame)
+            complete_frame.pack(anchor='w')
 
+            completed_label = Label(complete_frame, text=task_name)
+            completed_label.pack(side='left')
 
+            completed_date = Label(complete_frame, text=task_date)
+            completed_date.pack(side="left")
 
-    user_name = Label(root, text=f"welcome {username}", font=('Goudy Stout', 15))
-    user_name.pack(padx=5, pady=5)
     
+    def reload_task():
+        load_task()
+        display_completed()
+    
+    seperator_line = ttk.Separator(root, orient="horizontal")
+    seperator_line.pack(fill='x')
+
+    user_name = Label(root, text=f"welcome {username}", font=('Times New Roman', 15))
+    user_name.place(x=1700, y=5)
+
+    seperator_line_name = ttk.Separator(root, orient='horizontal')
+    seperator_line_name.place(x=0, y=40, width='2000')
+
     task_label = Label(root, text="Enter your task :", font=('Consolas', 12))
-    task_label.place(x=100, y = 50)
+    task_label.place(x=100, y = 60)
     # task_label.pack(padx=5, pady=0)
 
     task_entry = Entry(root, width=42, font=('Consolas', 12))
-    task_entry.place(x=300, y =50)
+    task_entry.place(x=300, y =60)
     #task_entry.pack(padx=5, pady=5)
 
     task_date_label = Label(root, text="Enter your date (yyyy-mm-dd):", font=('Consolas', 12))
-    task_date_label.place(x=800, y=50)
+    task_date_label.place(x=800, y=60)
     #task_date_label.pack(padx=5, pady=5)
 
     # task_date = DateEntry(root,date_pattern='yyyy/mm/dd' ,width=20, background='darkblue',foreground='white', borderwidth=2)
     # task_date.place(x=450, y=105)
     task_date_entry = Entry(root, width=20, font=('Consolas', 12))
-    task_date_entry.place(x=1130, y=50)
+    task_date_entry.place(x=1130, y=60)
     #task_date_entry.pack(padx=5, pady=5)
 
     task_add_btn = Button(root, text="add task", command=add_task, width=20)
-    task_add_btn.place(x=1400, y=45)
+    task_add_btn.place(x=1400, y=55)
     #task_add_btn.pack(padx=5, pady=5)
 
     today_frame = LabelFrame(root, text="today's task")
@@ -170,18 +197,17 @@ def task(username):  # main function
 
     tomorrow_frame = LabelFrame(root, text="upcoming task")
     #tomorrow_frame.pack(padx=5, pady=5)
-    tomorrow_frame.place(x=500, y = 100)
+    tomorrow_frame.place(x=700, y = 100)
 
     previous_frame = LabelFrame(root, text="incomplete previous task")
-    previous_frame.place(x=1000, y = 100)
+    previous_frame.place(x=1300, y = 100)
     #previous_frame.pack(padx=5, pady=5)
 
-    #complted_frame = LabelFrame(root,text="completed task")
-    # complted_frame.place(x=500, y=800)
+    complted_frame = LabelFrame(root,text="completed task")
+    complted_frame.place(x=30, y=800)
     #complted_frame.pack(side="right",pady=5, padx=5)
     
-    # display_completed()
-    load_task()
+    reload_task()
 
     root.mainloop()
     conn.close()
